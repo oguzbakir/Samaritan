44358c33a3576cc4590f2dcb6c5c571dc43d26ef
oguzbakir <b21627007@cs.hacettepe.edu.tr>
2017-06-05 20:51:50 +0300

    Successful test

--- core.py
+++ core.py
@@ -30,28 +30,12 @@ def findchannelbyid(id):
         if(channel["id"]==str(id)):
           return channel["name"]
 def analyze(data):
-  print(data)
   if(data["type"]=="message"):
     if("samaritan" in data["text"].lower()):
-      dertli=0
-      for elem in dertkeywords:
-        if(elem in data["text"].lower()):
-          textmessage=finduserbyid(data["user"])+", "+barmentavsiyeleri[random.randint(0,len(barmentavsiyeleri))]
-          sc.rtm_send_message(data["channel"],textmessage)
-          dertli=1
-          break
-      #print("Efendim , "+finduserbyid(data["user"]))
-      if(finduserbyid(data["user"])!="ze0bot" and not dertli):
+      if(finduserbyid(data["user"])!="samaritan"):
         textmessage="Efendim,  "+finduserbyid(data["user"])
         sc.rtm_send_message(data["channel"],textmessage)
-      dertli=0
-    elif(findchannelbyid(data["channel"])=="git"):
-      try:
-        if(data["bot_id"]==GITBOT):
-          textmessage="Oooo commit alırım bi dal"
-          sc.rtm_send_message("#botlamuhabbet",textmessage)
-      except:
-        pass
+    
 
     elif("py-dev" in data["text"].lower()):
       codetex=data["text"].lstrip("py-dev")
@@ -68,7 +52,7 @@ def analyze(data):
       else:
         textmessage="Output : \n"+ans.decode()
         sc.rtm_send_message(data["channel"],textmessage)
-    elif("youtube.com" in data["text"]):
+    elif("youtube.com/watch" in data["text"]):
       textmessage=videoanalyze(data["text"].lstrip("<").rstrip(">"))
       sc.rtm_send_message(data["channel"],textmessage)
 
@@ -77,14 +61,9 @@ def listen():
   if sc.rtm_connect():
       while True:
           data=sc.rtm_read()
+          print(data)
           if(data!=[]):
-            try:
-              print(data[0]["team"])
-              if(data[0]["team"]==TEAM):
-                #print("var böyle bi mesaj")
-                _thread.start_new_thread(analyze,(data[0],))
-            except:
-              nothing=0
+            _thread.start_new_thread(analyze,(data[0],))
           time.sleep(1)
   else:
       print("Connection Failed, invalid token?")
@@ -93,12 +72,8 @@ random.seed()
 conffile=open("slack.config","r")
 SLACK_BOT_TOKEN=conffile.readline().lstrip("SLACK_BOT_TOKEN=").rstrip()
 #print(SLACK_BOT_TOKEN)
-TEAM=conffile.readline().lstrip("TEAM").rstrip()
-TEAM=TEAM.lstrip("=")
+TEAM=conffile.readline().lstrip("TEAM=").rstrip()
 #print(TEAM)
-GITBOT=conffile.readline().lstrip("GITBOT=").rstrip()
-#print(GITBOT)
+
 sc = SlackClient(SLACK_BOT_TOKEN)
-barmentavsiyeleri=["Ya bir yol bul, ya bir yol aç, ya da yoldan çekil","En büyük savunma savaştır","The beauty is just an illusion","Bazen tutmak, bırakmaktan daha çok acıtır","Senden iyisini mi bulacak","Akışına bırak","Zaman her şeyin ilacıdır","Emeklerinin karşılığını alacaksın","Bir gün sen de mutlu olacaksın","Öldürmeyen şey güçlendirir","Günün en karanlık anları tan ağarmadan önceki dakikalardır","Günün en karanlık anları tan ağarmadan önceki dakikalardır","Act without expectation","Hayat provası olmayan bir oyundur","Karanlığa söveceğine bir mum yak","Derin olan kuyu değil, kısa olan iptir","Marifet hiç düşmemek değil, her düştüğünde kalkabilmektir","Alfred why do we fall","l'amour fait mal","Yavaş yürüyene çelme takılmaz"]
-dertkeywords=["dert","üzgünüm","üzgün","dertli","dertliyim","hüzün","hasret","acı"]
 listen()
